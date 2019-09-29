de43c710f33018b32de866320639432835133fab
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-09-29 14:28:12 +0200

    Added mail template

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 # SURFsara Lisa installation
 ############
 
-# Copy .forward for e-mailing from Lisa
+# Copy .forward for e-mailing from Lisa, add mail_template program to path
 cp .forward $HOME
+cp mail_template $HOME/.local/bin
 
 # Install zsh
 sh install_zsh.sh
 
 echo 'alias queue="squeue -u $(id -u)"' >> ~/.bashrc
 echo 'alias queue="squeue -u $(id -u)"' >> ~/.zshrc
+
+echo 'export TMPDIR=/tmp/$USER' >> ~/.bashrc
+echo 'export TMPDIR=/tmp/$USER' >> ~/.zshrc
 
 source ~/.bashrc
 source ~/.zshrc
